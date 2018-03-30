6fc9e191203ecdbf5e715fe6f39cee9ada12a139
Loc Tran <tomtran43@gmail.com>
2018-03-29 20:08:37 -0400

    Dockerized app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const routes = require("./routes");
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 80;
 
 const app = express();
 
